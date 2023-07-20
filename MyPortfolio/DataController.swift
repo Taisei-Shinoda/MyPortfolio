53bc12825f67a3902ac45d3342ee674b8fe7d234
shinoda-g-2 <aaatshinodaaat@gmail.com>
2023-07-20 16:22:46 +0900

    Filtering #1

--- MyPortfolio/DataController.swift
+++ MyPortfolio/DataController.swift
@@ -18,6 +18,25 @@ class DataController: ObservableObject {
     
     @Published var selectedIssue: Issue?
     
+    @Published var filterText = ""
+    
+    @Published var filterTokens = [Tag]()
+    
+    var suggestedFilterTokens: [Tag] {
+        guard filterText.starts(with: "#") else {
+            return []
+        }
+
+        let trimmedFilterText = String(filterText.dropFirst()).trimmingCharacters(in: .whitespaces)
+        let request = Tag.fetchRequest()
+
+        if trimmedFilterText.isEmpty == false {
+            request.predicate = NSPredicate(format: "name CONTAINS[c] %@", trimmedFilterText)
+        }
+
+        return (try? container.viewContext.fetch(request).sorted()) ?? []
+    }
+    
     static var preview: DataController = {
         let dataController = DataController(inMemory: true)
         dataController.createSampleData()
@@ -28,7 +47,7 @@ class DataController: ObservableObject {
     
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "Main")
-
+        
         if inMemory {
             container.persistentStoreDescriptions.first?.url = URL(filePath: "/dev/null")
         }
@@ -43,10 +62,10 @@ class DataController: ObservableObject {
          */
         /// 1. 管理対象のビューコンテキストのBooleanプロパティを有効にすることで、Core Dataに処理を任せることができる
         container.viewContext.automaticallyMergesChangesFromParent = true
-
+        
         /// 2.
         container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
-
+        
         /// 3. 最初のメソッドは、ストアが変更されたときに通知されるようにCore Dataに指示し、2番目のメソッドは、変更が発生するたびにメソッドを呼び出すようにシステムに指示
         container.persistentStoreDescriptions.first?.setOption(true as NSNumber, forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
         NotificationCenter.default.addObserver(forName: .NSPersistentStoreRemoteChange, object: container.persistentStoreCoordinator, queue: .main, using: remoteStoreChanged)
@@ -65,7 +84,7 @@ class DataController: ObservableObject {
     
     func queueSave() {
         saveTask?.cancel()
-
+        
         saveTask = Task { @MainActor in
             try await Task.sleep(for: .seconds(3))
             save()
@@ -75,22 +94,22 @@ class DataController: ObservableObject {
     func missingTags(from issue: Issue) -> [Tag] {
         let request = Tag.fetchRequest()
         let allTags = (try? container.viewContext.fetch(request)) ?? []
-
+        
         let allTagsSet = Set(allTags)
         let difference = allTagsSet.symmetricDifference(issue.issueTags)
-
+        
         return difference.sorted()
     }
     
     
     func createSampleData() {
         let viewContext = container.viewContext
-
+        
         for i in 1...5 {
             let tag = Tag(context: viewContext)
             tag.id = UUID()
             tag.name = "Tag \(i)"
-
+            
             for j in 1...10 {
                 let issue = Issue(context: viewContext)
                 issue.title = "Issue \(i)-\(j)"
@@ -101,7 +120,7 @@ class DataController: ObservableObject {
                 tag.addToIssues(issue)
             }
         }
-
+        
         try? viewContext.save()
     }
     
@@ -120,7 +139,7 @@ class DataController: ObservableObject {
     private func delete(_ fetchRequest: NSFetchRequest<NSFetchRequestResult>) {
         let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
         batchDeleteRequest.resultType = .resultTypeObjectIDs
-
+        
         if let delete = try? container.viewContext.execute(batchDeleteRequest) as? NSBatchDeleteResult {
             let changes = [NSDeletedObjectsKey: delete.result as? [NSManagedObjectID] ?? []]
             NSManagedObjectContext.mergeChanges(fromRemoteContextSave: changes, into: [container.viewContext])
@@ -130,13 +149,44 @@ class DataController: ObservableObject {
     func deleteAll() {
         let request1: NSFetchRequest<NSFetchRequestResult> = Tag.fetchRequest()
         delete(request1)
-
+        
         let request2: NSFetchRequest<NSFetchRequestResult> = Issue.fetchRequest()
         delete(request2)
-
+        
         save()
     }
     
-    
+    func issuesForSelectedFilter() -> [Issue] {
+        let filter = selectedFilter ?? .all
+        var predicates = [NSPredicate]()
+
+        if let tag = filter.tag {
+            let tagPredicate = NSPredicate(format: "tags CONTAINS %@", tag)
+            predicates.append(tagPredicate)
+        } else {
+            let datePredicate = NSPredicate(format: "modificationDate > %@", filter.minModificationDate as NSDate)
+            predicates.append(datePredicate)
+        }
+
+        let trimmedFilterText = filterText.trimmingCharacters(in: .whitespaces)
+
+        if trimmedFilterText.isEmpty == false {
+            let titlePredicate = NSPredicate(format: "title CONTAINS[c] %@", trimmedFilterText)
+            let contentPredicate = NSPredicate(format: "content CONTAINS[c] %@", trimmedFilterText)
+            let combinedPredicate = NSCompoundPredicate(orPredicateWithSubpredicates: [titlePredicate, contentPredicate])
+            predicates.append(combinedPredicate)
+        }
+        
+        if filterTokens.isEmpty == false {
+            let tokenPredicate = NSPredicate(format: "ANY tags IN %@", filterTokens)
+            predicates.append(tokenPredicate)
+        }
+        
+        let request = Issue.fetchRequest()
+        request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
+
+        let allIssues = (try? container.viewContext.fetch(request)) ?? []
+        return allIssues.sorted()
+    }
     
 }
