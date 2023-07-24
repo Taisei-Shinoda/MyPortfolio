763bcf58da6c12983ee40bb2b250f5dc8129a749
shinoda-g-2 <aaatshinodaaat@gmail.com>
2023-07-24 22:07:35 +0900

    UI Testing

--- MyPortfolio/DataController.swift
+++ MyPortfolio/DataController.swift
@@ -8,6 +8,7 @@
 
 import CoreData
 import Foundation
+import UIKit
 
 
 enum SortType: String {
@@ -31,11 +32,11 @@ class DataController: ObservableObject {
         guard let url = Bundle.main.url(forResource: "Main", withExtension: "momd") else {
             fatalError("Failed to locate model file.")
         }
-
+        
         guard let managedObjectModel = NSManagedObjectModel(contentsOf: url) else {
             fatalError("Failed to load model file.")
         }
-
+        
         return managedObjectModel
     }()
     
@@ -56,14 +57,14 @@ class DataController: ObservableObject {
         guard filterText.starts(with: "#") else {
             return []
         }
-
+        
         let trimmedFilterText = String(filterText.dropFirst()).trimmingCharacters(in: .whitespaces)
         let request = Tag.fetchRequest()
-
+        
         if trimmedFilterText.isEmpty == false {
             request.predicate = NSPredicate(format: "name CONTAINS[c] %@", trimmedFilterText)
         }
-
+        
         return (try? container.viewContext.fetch(request).sorted()) ?? []
     }
     
@@ -80,7 +81,7 @@ class DataController: ObservableObject {
     /// - Parameter inMemory: データを一時メモリに保存するかどうか
     init(inMemory: Bool = false) {
         
-//        container = NSPersistentCloudKitContainer(name: "Main")
+        //        container = NSPersistentCloudKitContainer(name: "Main")
         /// Core Data がエンティティを登録する際の競合を防ぐため [2]
         container = NSPersistentCloudKitContainer(name: "Main", managedObjectModel: Self.model)
         
@@ -114,9 +115,20 @@ class DataController: ObservableObject {
             using: remoteStoreChanged)
         
         container.loadPersistentStores { _, error in
+            /// 成功または失敗にかかわらず、コア データ スタックの読み込みが完了したことを意味
             if let error {
                 fatalError("ロードに失敗: \(error.localizedDescription)")
             }
+            /// デバッグ モード
+            /// UI テスト ターゲット用であり、アプリがテスト環境にあることを認識できるようにその起動引数を認識し、既存のデータを削除してその構成に応答するコードを追加
+            #if DEBUG
+            if CommandLine.arguments.contains("enable-testing") {
+                self.deleteAll()
+                /// アプリのすべてのアニメーションが無効になり、UI テストが大幅に高速化される
+                UIView.setAnimationsEnabled(false)
+            }
+            #endif
+            
         }
     }
     
@@ -203,7 +215,7 @@ class DataController: ObservableObject {
     func issuesForSelectedFilter() -> [Issue] {
         let filter = selectedFilter ?? .all
         var predicates = [NSPredicate]()
-
+        
         if let tag = filter.tag {
             let tagPredicate = NSPredicate(format: "tags CONTAINS %@", tag)
             predicates.append(tagPredicate)
@@ -211,9 +223,9 @@ class DataController: ObservableObject {
             let datePredicate = NSPredicate(format: "modificationDate > %@", filter.minModificationDate as NSDate)
             predicates.append(datePredicate)
         }
-
+        
         let trimmedFilterText = filterText.trimmingCharacters(in: .whitespaces)
-
+        
         /// フィルタリングの述語
         if trimmedFilterText.isEmpty == false {
             let titlePredicate = NSPredicate(format: "title CONTAINS[c] %@", trimmedFilterText)
@@ -238,7 +250,7 @@ class DataController: ObservableObject {
                 let priorityFilter = NSPredicate(format: "priority = %d", filterPriority)
                 predicates.append(priorityFilter)
             }
-
+            
             if filterStatus != .all {
                 let lookForClosed = filterStatus == .closed
                 let statusFilter = NSPredicate(format: "completed = %@", NSNumber(value: lookForClosed))
@@ -250,7 +262,7 @@ class DataController: ObservableObject {
         let request = Issue.fetchRequest()
         request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
         request.sortDescriptors = [NSSortDescriptor(key: sortType.rawValue, ascending: sortNewestFirst)]
-
+        
         let allIssues = (try? container.viewContext.fetch(request)) ?? []
         return allIssues
     }
@@ -279,7 +291,7 @@ class DataController: ObservableObject {
     func count<T>(for fetchRequest: NSFetchRequest<T>) -> Int {
         (try? container.viewContext.count(for: fetchRequest)) ?? 0
     }
-
+    
     
     func hasEarned(award: Award) -> Bool {
         switch award.criterion {
@@ -287,18 +299,18 @@ class DataController: ObservableObject {
             let fetchRequest = Issue.fetchRequest()
             let awardCount = count(for: fetchRequest)
             return awardCount >= award.value
-
+            
         case "closed":
             let fetchRequest = Issue.fetchRequest()
             fetchRequest.predicate = NSPredicate(format: "completed = true")
             let awardCount = count(for: fetchRequest)
             return awardCount >= award.value
-
+            
         case "tags":
             let fetchRequest = Tag.fetchRequest()
             let awardCount = count(for: fetchRequest)
             return awardCount >= award.value
-
+            
         default:
             return false
         }
