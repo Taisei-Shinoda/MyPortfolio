8c0f607f9b3ef3e01244714c84172a0f437fe483
Taisei Shinoda <aaatshinodaaat@gmail.com>
2023-10-27 18:18:59 +0900

    Datacontroller の拡張

---
+++ MyPortfolio/Extensions/DataController-Notifications.swift
@@ -0,0 +1,18 @@
+//
+//  DataController-Notifications.swift
+//  MyPortfolio
+//
+//  Created by Taisei Shinoda on 2023/10/27.
+//
+
+import Foundation
+
+
+
+extension DataController {
+    func addReminder(for issue: Issue) async -> Bool {
+        
+        
+    }
+    
+}
